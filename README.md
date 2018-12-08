0e520e9a2073022af2daa018d65f47744da71c1a
Emil Söderlind <id15esd@cs.umu.se>
2018-12-08 23:34:23 +0100

    refactor + optimizing code + typo

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # TrissSimulatorScript
-Python scripts which simulates skrejping Triss
+_Simulating Triss lottery tickets, which you skrejp one by one. Simulating real Triss:es with correct cost and prize._
+
+## Basic usage
+
+```bash
+# Starting the simulator
+python3 TrissSimulator.py 
+
+```
+
+1. Generates 2.000.000 Triss (Takes a while)
+2. Scrumbles the Triss:es
+3. By pressing the [Enter]-key you can skrejp as many Triss:es your heart desire.
+4. When you are done, write anything and press the [Enter]-key.
+5. You are presented with a skrejping summary.
+
+## Example
+
+```bash
+
+python3 TrissSimulator.py 
+Creating 2.000.000 Triss:es
+| 100.0%
+| Scrambeling.
+
+Do you want to skrejp a triss?
+(Press [Enter] to skrejp or write something to exit)
+
+
+------- Triss #1 -------
+You won 0 SEK!
+------- Triss #1 -------
+
+
+Do you want to skrejp a triss?
+(Press [Enter] to skrejp or write something to exit)
+
+
+------- Triss #2 -------
+You won 60 SEK!
+------- Triss #2 -------
+
+
+Do you want to skrejp a triss?
+(Press [Enter] to skrejp or write something to exit)
+
+
+------- Triss #3 -------
+You won 0 SEK!
+------- Triss #3 -------
+
+
+Do you want to skrejp a triss?
+(Press [Enter] to skrejp or write something to exit)
+exit
+
+---- Final skrejping stats ----
+Skrejped: 3 Triss:es
+Spent: 90 SEK
+Won: 60 SEK
+Loss: -30 SEK
+---- Skrejping stats ----
+
+Thank you, come again!
+
+```
--- TrissSimulator.py
+++ TrissSimulator.py
@@ -30,53 +30,117 @@ def createTrissList():
         sys.stdout.write("\r| {}%".format(round(((trissNumber+1)/2000000)*100,1)))
 
         # Set each triss prize
-        if(trissNumber == 0):
-            currTriss.setPrize(2765000)
-        elif(trissNumber == 1):
-            currTriss.setPrize(1000000)
-        elif(trissNumber >= 2 and trissNumber <= 6):
-            currTriss.setPrize(265000)
-        elif(trissNumber == 7):
-            currTriss.setPrize(200000)
-        elif(trissNumber >= 8 and trissNumber <= 9):
-            currTriss.setPrize(100000)
-        elif(trissNumber >= 10 and trissNumber <= 11):
-            currTriss.setPrize(20000)
-        elif(trissNumber >= 12 and trissNumber <= 51):
-            currTriss.setPrize(10000)
-        elif(trissNumber >= 52 and trissNumber <= 81):
-            currTriss.setPrize(5000)
-        elif(trissNumber >= 82 and trissNumber <= 151):
-            currTriss.setPrize(2000)
-        elif(trissNumber >= 152 and trissNumber <= 231):
-            currTriss.setPrize(1500)
-        elif(trissNumber >= 232 and trissNumber <= 396):
-            currTriss.setPrize(1000)
-        elif(trissNumber >= 397 and trissNumber <= 496):
-            currTriss.setPrize(900)
-        elif(trissNumber >= 497 and trissNumber <= 696):
-            currTriss.setPrize(600)
-        elif(trissNumber >= 697 and trissNumber <= 1006):
-            currTriss.setPrize(500)
-        elif(trissNumber >= 1007 and trissNumber <= 1959):
-            currTriss.setPrize(300)
-        elif(trissNumber >= 1960 and trissNumber <= 3159):
-            currTriss.setPrize(180)
-        elif(trissNumber >= 3160 and trissNumber <= 6919):
-            currTriss.setPrize(150)
-        elif(trissNumber >= 6920 and trissNumber <= 14119):
-            currTriss.setPrize(120)
-        elif(trissNumber >= 14120 and trissNumber <= 43919):
-            currTriss.setPrize(90)
-        elif(trissNumber >= 43920 and trissNumber <= 252719):
-            currTriss.setPrize(60)
+        if(trissNumber > 432419):
+            currTriss.setPrize(0)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
         elif(trissNumber >= 252720 and trissNumber <= 432419):
             currTriss.setPrize(30)
-        else:
-            currTriss.setPrize(0)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 43920 and trissNumber <= 252719):
+            currTriss.setPrize(60)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 14120 and trissNumber <= 43919):
+            currTriss.setPrize(90)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 6920 and trissNumber <= 14119):
+            currTriss.setPrize(120)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 3160 and trissNumber <= 6919):
+            currTriss.setPrize(150)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 1960 and trissNumber <= 3159):
+            currTriss.setPrize(180)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 1007 and trissNumber <= 1959):
+            currTriss.setPrize(300)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 697 and trissNumber <= 1006):
+            currTriss.setPrize(500)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 497 and trissNumber <= 696):
+            currTriss.setPrize(600)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 397 and trissNumber <= 496):
+            currTriss.setPrize(900)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 232 and trissNumber <= 396):
+            currTriss.setPrize(1000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 152 and trissNumber <= 231):
+            currTriss.setPrize(1500)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 82 and trissNumber <= 151):
+            currTriss.setPrize(2000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 52 and trissNumber <= 81):
+            currTriss.setPrize(5000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 12 and trissNumber <= 51):
+            currTriss.setPrize(10000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 10 and trissNumber <= 11):
+            currTriss.setPrize(20000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 8 and trissNumber <= 9):
+            currTriss.setPrize(100000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber == 7):
+            currTriss.setPrize(200000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber >= 2 and trissNumber <= 6):
+            currTriss.setPrize(265000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber == 1):
+            currTriss.setPrize(1000000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
+        elif(trissNumber == 0):
+            currTriss.setPrize(2765000)
+            # Append triss to list
+            trissList.append(currTriss)
+            continue
 
-        # Append trisses to list
-        trissList.append(currTriss)
     return trissList
 
 def scrambleList(list):
@@ -84,31 +148,14 @@ def scrambleList(list):
     random.shuffle(dest)
     return dest
 
-# Old function
-def trissUntillBestPrice():
-    notFoundMax = True
-    i = 0
-    while(notFoundMax):
-        i = i + 1
-        r = randint(0,1999999)
-        if(r == 0):
-            print("STORVINST 2.765 miljoner SEK!", "Efter {} lotter.".format(i))
-            print("Kostnad: {} SEK".format(i*30))
-            notFoundMax = False
-            return i
-        else:
-            print("Lott #{}. Ingen storvinst. Kostnad: {} SEK".format(i,i*30))
-
-
-
 def main():
 
     # Constants
     skrejpTime = 4
 
-    print("Creating 2.000.000 Triss:es")
+    print("Creating 2.000.000 tickets")
     filledList = createTrissList()
-    print("\n| Scrambeling triss:es.\n")
+    print("\n| Scrambeling.\n")
     scrambledList = scrambleList(filledList)
 
     prizeSum = 0
@@ -142,7 +189,7 @@ def main():
     else:
         print("Loss: {} SEK".format(prizeSum-(trissIndex*30)))
 
-    print("---- Skrejping stats ----")
+    print("---- Final skrejping stats ----")
 
 
     print("\nThank you, come again!")
